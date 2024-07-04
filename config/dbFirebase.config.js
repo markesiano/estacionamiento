0384dcb6772cc3c2c1dfb081f9ab93089f8f308e
Jesús Eduardo Jiménez García <markesiano@live.com.mx>
2024-07-03 20:57:01 -0600

    Update dbFirebase.config.js

--- config/dbFirebase.config.js
+++ config/dbFirebase.config.js
@@ -1,12 +1,7 @@
 export class dbFirebaseConfig {
 
     static firebaseConfig = {
-        apiKey: "AIzaSyDLLsK27heDV8WYHeWymbqt_m3mak9agmg",
-        authDomain: "estacionamiento-f3fb8.firebaseapp.com",
-        projectId: "estacionamiento-f3fb8",
-        storageBucket: "estacionamiento-f3fb8.appspot.com",
-        messagingSenderId: "945067152443",
-        appId: "1:945067152443:web:2cf063c2f50b36995eb81e"
+
     };
 
 }
